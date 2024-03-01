1a6ef8b1149f0fdee8b881c9ebb16dcb46f21660
MOA -CODES <moyo.ajla@gmail.com>
2024-03-01 09:26:55 +0100

    connection file fix

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
 
     try{
-        return (await mongoose.connect(process.env.MONGO_URI)).isObjectIdOrHexString(()=>console.log('connected to MongoDb'))
+        return (await mongoose.connect(process.env.MONGO_URI)).then(()=>console.log('connected to MongoDb'))
     }catch(err){
         console.log(err).then(()=>process.exit(1))
     }
